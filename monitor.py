f37bbf1708339a7e3e86b4f3a2c3adc247edeb24
Haavard Fagervoll <fagervoll@gmail.com>
2016-09-16 20:28:53 +0200

    simple monitor fucntions.

--- monitor.py
+++ monitor.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+debug = 1
+
 
 import subprocess
 
-def createcommand(command):
-    finished_command = []
-    finished_command = command.split(" ")
+get_load = "snmpwalk -c public -v 2c localhost:161 .1.3.6.1.4.1.2021.10.1.3"
+get_temp = "/opt/vc/bin/vcgencmd measure_temp"
 
-request_temp = '''/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//g' | sed "s/'.*//g"'''
+get_load_ubuntu = "snmpwalk -c public -v 2c localhost:161 .1.3.6.1"
 
-temp = subprocess.Popen(request_temp.split(" "), stdout=subprocess.PIPE)
-output = temp.communicate()[0]
+def command(command):
+    temp = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+    output = temp.communicate()[0]
+    return output
+
+
+def main():
+    if debug:
+        print(command(get_load_ubuntu))
+    else:
+        print command(get_load)
+        print command(get_temp)
+
+if __name__ == "__main__":
+    main()
