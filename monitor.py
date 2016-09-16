3928af42234714b2496145bb481f8c190cfa075e
Haavard Fagervoll <fagervoll@gmail.com>
2016-09-16 19:20:18 +0200

    noe

--- monitor.py
+++ monitor.py
@@ -3,9 +3,11 @@
 
 import subprocess
 
+def createcommand(command):
+    finished_command = []
+    finished_command = command.split(" ")
+
 request_temp = '''/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//g' | sed "s/'.*//g"'''
 
-temp = subprocess.Popen(request_temp)
-
-print temp
-
+temp = subprocess.Popen(request_temp.split(" "), stdout=subprocess.PIPE)
+output = temp.communicate()[0]
