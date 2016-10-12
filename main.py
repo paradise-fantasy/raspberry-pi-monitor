155b3a6f5a222d7ccae7cd1294744098b6bc0c2b
Server <an@email>
2016-10-12 12:39:04 +0000

    Adds logging

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from api import postData
 from snmp_fetcher import fetchData
 import time
+import logging
+
+logging.basicConfig(filename="logs/output.log", level=logging.DEBUG)
 
 if __name__ == "__main__":
-    print "rpi-vitals-monitor started"
+    print "[*] Raspberry-vitals-monitor started"
     while True:
 	snmpData = fetchData();
-	print "fetched SNMP data"
+	print "[*] Fetched SNMP data"
 	for field, value in snmpData.iteritems():
-	    print "%s: %f" % (field, value);
-        postData(fetchData());
-	print "posted data to API"
+	    print "[*] %s: %f" % (field, value);
+        postData(snmpData);
+	print "[*] Posted data to API"
         time.sleep(15)
